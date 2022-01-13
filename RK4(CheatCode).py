f8ca0042b2605a4292e0133c8da6b3f1346d88eb
Dharshannan Sugunan <94248626+Dharshannan@users.noreply.github.com>
2022-01-13 22:12:17 +0000

    Update RK4(CheatCode).py

--- RK4(CheatCode).py
+++ RK4(CheatCode).py
@@ -10,25 +10,24 @@ A = np.array([[0,1],[-9,-0.1]])
 
 
 temp = y_0
-for j in range(0, len(y_0)):
-   y_poop = []
-   for k in range(len(y_0)):
-       y_poop.append([])
-       
-   for n in range(0,len(y_0)):
-       y_poop[n].append(y_0[n])
-   temp = y_0   
-   for i in range(0, len(t)-1):
-       k1 = h*np.matmul(A,temp)
-       k2 = h*np.matmul(A,temp + 0.5*k1)
-       k3 = h*np.matmul(A,temp + 0.5*k2)
-       k4 = h*np.matmul(A,temp + k3)
-       y_t = temp + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-       print(k1,k2,k3,k4,y_t)
-    
-       temp = y_t
-       for j in range(0, len(y_0)):
-           y_poop[j].append(temp[j])
+y_poop = []
+for k in range(len(y_0)):
+    y_poop.append([])
+
+for n in range(0,len(y_0)):
+    y_poop[n].append(y_0[n])
+ 
+for i in range(0, len(t)-1):
+    k1 = h*np.matmul(A,temp)
+    k2 = h*np.matmul(A,temp + 0.5*k1)
+    k3 = h*np.matmul(A,temp + 0.5*k2)
+    k4 = h*np.matmul(A,temp + k3)
+    y_t = temp + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+    print(k1,k2,k3,k4,y_t)
+
+    temp = y_t
+    for j in range(0, len(y_0)):
+        y_poop[j].append(temp[j])
 
         
 for i in range(0,len(y_0)):
