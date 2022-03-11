a94c2c6a3a440d8af22656e94c0ac232c05c63cb
Dharshannan Sugunan <94248626+Dharshannan@users.noreply.github.com>
2022-03-11 19:59:17 +0000

    Add files via upload

--- SchrodingerWave_DoubleSlit.py
+++ SchrodingerWave_DoubleSlit.py
@@ -5,14 +5,14 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Rectangle
 
 # We are coding a Crank-Nicolson Method to solve the Schrodinger Wave equation for a wave packet propagating through a double slit
-
+# Assume the wave particles are confined within an infinite square well
 def psi0(x, y, x0, y0, sigma=0.5, k=15*np.pi):
     # Initial state
-    return np.exp(-1/2*((x-x0)**2 + (y-y0)**2)/sigma**2)*np.exp(1j*k*(x-x0)) # Gaussian Wave packet
+    return ((1/(2*np.pi*sigma**2))**(0.25))*np.exp(-1/4*((x-x0)**2 + (y-y0)**2)/sigma**2)*np.exp(1j*k*(x-x0)) # Gaussian Wave packet
 
 def doubleSlit_interaction(psi, j0, j1, i0, i1, i2, i3):
     psi = np.asarray(psi) 
-    # We cancel the wave function inside the walls of the double slit.
+    # cancel the wave function inside the walls of the double slit.
     psi[0:i3, j0:j1] = 0
     psi[i2:i1,j0:j1] = 0
     psi[i0:,  j0:j1] = 0
@@ -54,7 +54,7 @@ y = np.linspace(0, L, Ny-2)
 x, y = np.meshgrid(x, y)
 psis = [] # To store the wave function at each time step.
 
-psi = psi0(x, y, x0, y0) # We initialise the wave function with the Gaussian.
+psi = psi0(x, y, x0, y0) # initialise the wave function with the Gaussian.
 psi[0,:] = psi[-1,:] = psi[:,0] = psi[:,-1] = 0 # The wave function equals 0 at the edges of the simulation box (infinite potential well).
 psi = doubleSlit_interaction(psi, j0, j1, i0, i1, i2, i3) # Initial interaction with the double slit.
 psis.append(np.copy(psi))
@@ -67,22 +67,25 @@ for i in range(0,m):
         eva.append((i,j))
         
 k = m*n # Number of points to evaluate
-A = np.zeros((k,k), complex)
-M = np.zeros((k,k), complex)
+A = np.zeros((k,k), complex) # Coefficient matrix of the LHS of the discretised wave equation
+M = np.zeros((k,k), complex) # Coefficient matrix of the RHS of the discretised wave equation
 
 # Populate A and M
 coeff = []
 for i in range(0,m):
     for j in range(0,n):
+        # append the index of psi on both LHS and RHS of the equation for each i,j values
         coeff.append([(i,j), (i+1,j), (i-1,j), (i,j+1), (i,j-1)])
       
 for i in range(0,k):
     for j in range(0,k):
         if eva[j] in coeff[i]:
+            # Populate the matrix of coefficients
             A[i][j] = -1*r
-            M[i][j] = 1*r
+            M[i][j] =  1*r
             
 for i in range(0,k):
+    # Update the diagonal of the matrix of coefficients
     A[i][i] = (1 + 4*r)
     M[i][i] = (1 - 4*r)
 
@@ -96,44 +99,44 @@ from scipy.sparse.linalg import spsolve
 Asp = csc_matrix(A)
 
 for s in range(1,Nt):     
-    vec = psi.reshape((k))
-    b = np.matmul(M,vec)
-    vec = spsolve(Asp,b)
-    psi = vec.reshape((Nx-2,Ny-2))
-    psi = doubleSlit_interaction(psi, j0, j1, i0, i1, i2, i3)
+    vec = psi.reshape((k)) # reshape the psi matrix into a column vector
+    b = np.matmul(M,vec) # compute the RHS column b by matrix multiplying M and vec
+    vec = spsolve(Asp,b) # Solve the matrix for psi values
+    psi = vec.reshape((Nx-2,Ny-2)) # reshape new psi values from column to matrix form
+    psi = doubleSlit_interaction(psi, j0, j1, i0, i1, i2, i3) # update the psi values with the interaction at the double slit walls
     psis.append(np.copy(psi))
     
-# We calculate the modulus of the wave function at each time step.
-mod_psis = [] # For storing the modulus of the wave function at each time step.
+# calculate the modulus of the wave function at each time step.
+mod_psis = [] # storing the modulus of the wave function at each time step.
 for wavefunc in psis:
-    mod = np.absolute(wavefunc) # We calculate the modulus of the wavefunc values that are complex.
-    mod_psis.append(mod) # We save the calculated modulus.
+    mod = np.absolute(wavefunc) # calculate the modulus of the wavefunc values that are complex.
+    mod_psis.append(mod) # save the calculated modulus.
     
-## In case there is need to save memory.
+## In case there is need to save memory ##
 del psis
 del M
 del vec
 
 ## Animation ## 
-fig = plt.figure() # We create the figure.
-ax = fig.add_subplot(111, xlim=(0,L), ylim=(0,L)) # We add the subplot to the figure.
+fig = plt.figure() # create the figure.
+ax = fig.add_subplot(111, xlim=(0,L), ylim=(0,L)) # add the subplot to the figure.
 
-img = ax.imshow(mod_psis[0], extent=[0,L,0,L], cmap=plt.get_cmap("hot"), vmin=0, vmax=np.max(mod_psis), zorder=1, interpolation="none") # Here the modulus of the 2D wave function shall be represented.
+img = ax.imshow(mod_psis[0], extent=[0,L,0,L], cmap=plt.get_cmap("hot"), vmin=0, vmax=np.max(mod_psis), zorder=1, interpolation="none") # here the modulus of the 2D wave function shall be represented.
 
-# We paint the walls of the double slit with rectangles.
+# paint the walls of the double slit with rectangles.
 wall_bottom = Rectangle((j0*Dy,0),     w, i3*Dy,      color="w", zorder=50) # (x0, y0), width, height
 wall_middle = Rectangle((j0*Dy,i2*Dy), w, (i1-i2)*Dy, color="w", zorder=50)
 wall_top    = Rectangle((j0*Dy,i0*Dy), w, i3*Dy,      color="w", zorder=50)
 
-# We add the rectangular patches to the plot.
+# add the rectangular patches to the plot.
 ax.add_patch(wall_bottom)
 ax.add_patch(wall_middle)
 ax.add_patch(wall_top)
 
-# We define the animation function for FuncAnimation.
+# define the animation function for FuncAnimation.
 
 def animate(i):
-    img.set_data(mod_psis[i]) # Fill img with the modulus data of the wave function.
+    img.set_data(mod_psis[i]) # fill img with the modulus data of the wave function.
     img.set_zorder(1)
     return img,
 
@@ -142,7 +145,7 @@ anim = FuncAnimation(fig, animate, interval=1, frames=np.arange(0,Nt,2), repeat=
 cbar = fig.colorbar(img)
 plt.show()
 
-## Save the animation
+## Save the animation ##
 '''f = r"D:\ME2MCP\PDEs\Anim.gif" 
 writergif = animation.PillowWriter(fps=30) 
 anim.save(f, writer=writergif)'''
